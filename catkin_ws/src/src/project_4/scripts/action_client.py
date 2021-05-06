f386df49a37ae4a716e97d703cedde838ccd9784
JB <48212794+josuebecerra7@users.noreply.github.com>
2021-05-06 20:08:39 +0100

    fix action client
    fix client

---
+++ catkin_ws/src/src/project_4/scripts/action_client.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Josue Becerra Rico
+# Intelligent Systems and Robotics
+
+# Import Libraries  
+import rospy
+import actionlib
+from project_4.msg import Navigate2DAction, Navigate2DFeedback, Navigate2DResult
+from geometry_msgs.msg import Point
